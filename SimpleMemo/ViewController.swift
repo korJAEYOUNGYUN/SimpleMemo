0276376e4d9d87c7cfe05b2373062dacbd4e68d0
JAEYOUNGYUN <lastmore9@gmail.com>
2020-11-18 17:54:05 +0900

    Initial Commit

---
+++ SimpleMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMemo
+//
+//  Created by jaeyoung Yun on 2020/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
